f478ca1a3734c9cacd920808f4ee7ed12b9f7a6f
Alikhan <maratbekovalikhan@gmail.com>
2026-02-13 11:40:36 +0600

    Remove .env from repo

--- backend/models/user.go
+++ backend/models/user.go
@@ -2,14 +2,6 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-// CourseProgress хранит прогресс по каждому курсу
-type CourseProgress struct {
-	Theory    bool `bson:"theory" json:"theory"`
-	Examples  bool `bson:"examples" json:"examples"`
-	TestScore int  `bson:"testScore" json:"testScore"`
-}
-
-// User хранит данные пользователя
 type User struct {
 	ID       primitive.ObjectID        `bson:"_id,omitempty" json:"id"`
 	Name     string                    `bson:"name" json:"name"`
